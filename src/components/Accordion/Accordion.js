7ec37cce9372b9d66f7d4b6f907dc920d229d3ec
Zenitude <mollet.rudy@gmail.com>
2022-11-16 13:58:27 +0100

    delete log console

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -15,7 +15,6 @@ export default function Accordion() {
 
     useEffect(() => {
         setHeightText(`${heightRefP.current.scrollHeight + 18}`)
-        console.log(heightRefP);
     }, [])
 
     const styleText = {
