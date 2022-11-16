21660ce043b882bf18a9e7dd212c632a8a387dbe
Zenitude <mollet.rudy@gmail.com>
2022-11-16 13:57:37 +0100

    add transition opacity and height

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Accordion from '../components/Accordion/Accordion';
 
 export default function App() {
+
   return (
     <div className="App">
-      
+      <Accordion />
     </div>
   );
 }
---
+++ src/components/Accordion/Accordion.js
@@ -0,0 +1,38 @@
+import {useState, useEffect, useRef, React} from 'react';
+import chevron from '../../assets/images/right-chevron.png';
+import './Accordion.css';
+
+export default function Accordion() {
+
+    const [toggle, setToggle] = useState(false);
+    const [heightText, setHeightText] = useState();
+
+    const heightRefP = useRef();
+
+    const changeToggle = () => {
+        setToggle(!toggle);
+    }
+
+    useEffect(() => {
+        setHeightText(`${heightRefP.current.scrollHeight + 18}`)
+        console.log(heightRefP);
+    }, [])
+
+    const styleText = {
+        height: toggle ? `${heightText}px` : "0px",
+    }
+
+    const rotateChevron = {
+        transform : toggle ? 'rotate(90deg)' : 'rotate(0deg)',
+    }
+
+    return (
+        <div className='accordion'>
+            <button onClick={changeToggle}>Afficher / Masquer <img src={chevron} style={rotateChevron}/></button>
+            <p ref={heightRefP} style={styleText} className={toggle ? "textAccordion activeAccord" : "textAccordion"}>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. At itaque repudiandae, atque sit excepturi voluptas. Accusantium quidem dolore modi exercitationem consequuntur doloremque illo sapiente vitae sequi? Exercitationem quas cumque consectetur soluta delectus dolorum? Fugiat magnam tempore, qui itaque, esse recusandae voluptate iure voluptatum dolore sint sapiente, minus dolorem nulla officia.
+            </p>
+            
+        </div>
+    )
+}
